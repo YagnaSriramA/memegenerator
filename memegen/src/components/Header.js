932bc0de864559c7c603fc6e6c4ed519a80dc340
Yagna Amballa <yagna.s.amballa@kipi.bi>
2023-04-09 23:57:34 +0530

    done with header ig

--- memegen/src/components/Header.js
+++ memegen/src/components/Header.js
@@ -1,2 +1,18 @@
 import React from "react"
 import '../style.css'
+import logo from '../images/troll-face.png'
+
+function Header()
+{
+    return(
+        <nav className="navbar">
+            <img src={logo} alt="memegen-png"></img>
+            <p className="memegen">Meme Generator</p>
+            <p className="react">React Course - Project 3</p>
+            
+            
+        </nav>
+    )
+}
+
+export default Header
---
+++ memegen/src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react"
+import '../style.css'
+
+function Meme()
+{
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Meme
