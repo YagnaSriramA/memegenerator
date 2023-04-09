79ecbe7b7b50a1f64ff2a96e75e997dc9059b012
Yagna Amballa <yagna.s.amballa@kipi.bi>
2023-04-10 00:25:06 +0530

    added the static parts of the meme component.

--- memegen/src/components/Meme.js
+++ memegen/src/components/Meme.js
@@ -5,7 +5,15 @@ function Meme()
 {
     return(
         <div>
-            
+            <form>
+                <div className="typeboxes">
+                    <input type="text" className="typebox--1"></input>
+                    <input type="text" className="typebox--2"></input>
+                </div>
+                <div className="submit-btn">
+                <input type="submit" value="Get a new meme image" ></input>
+                </div>
+            </form>
         </div>
     )
 }
