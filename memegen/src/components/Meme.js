b9adc5c71b472b8158283bdcaf5e3c7bc7cd9569
Yagna Amballa <yagna.s.amballa@kipi.bi>
2023-04-11 16:03:25 +0530

    added an event listener to pick a url in random from memesdata.js on clicking the get a new meme image button

--- memegen/src/components/Meme.js
+++ memegen/src/components/Meme.js
@@ -1,19 +1,37 @@
 import React from "react"
 import '../style.css'
+import data from '../memesData'
+
 
 function Meme()
 {
+    
+    
+    function getRandomInt(min,max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+
+    function randomeme(){
+        console.log(data.data.memes[getRandomInt(1,101)].url)
+
+    }
+    
+
+
     return(
         <div>
-            <form>
+            <div className="form">
                 <div className="typeboxes">
                     <input type="text" className="typebox--1"></input>
                     <input type="text" className="typebox--2"></input>
                 </div>
                 <div className="submit-btn">
-                <input type="submit" value="Get a new meme image" ></input>
+                <button onClick={randomeme}>Get a new meme image</button>
                 </div>
-            </form>
+            </div>
         </div>
     )
 }
